525592ce115c0df51e8a858067b4091a3ffc94a1
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-04 10:27:44 -0400

    created components in the corresponding files

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Some title</h1>
+      <p>Some content</p>
+    </div>
+  );
+}
+
+export default Note;
