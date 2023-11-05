fa0d8d2d6e42021a514c868db691b3a90732a900
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-04 22:26:29 -0400

    notes.js has content to be posted on the interface

---
+++ src/notes.js
@@ -0,0 +1,27 @@
+const notes = [
+    {
+      key: 1,
+      title: "for vs for each",
+      content:
+        "The choice between for and forEach depends on your specific use case. If you need fine-grained control over the iteration process, a for loop may be more appropriate. If you want a more straightforward way to iterate through all elements in an array, forEach is a good choice."
+    },
+    {
+      key: 2,
+      title: "React Developer Tools",
+      content:
+        "A chrome extension for viewing react components"
+    },
+    {
+      key: 3,
+      title: "Arrays",
+      content:
+        "Q. Why did the programmer quit his job? A. Because he didn't get arrays."
+    },
+    {
+      key: 4,
+      title: "Hardware vs Software",
+      content:
+        "You can hit your hardware with a hammer, but you can only curse at your software. Lol!!"
+    }
+  ];
+  
