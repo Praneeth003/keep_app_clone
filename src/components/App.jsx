e8cb756a254fc4706daa866df0f0c603bda60bb5
Praneeth003 <ch.krishnapraneeth@gmail.com>
2023-11-09 18:44:34 -0500

    style changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import Create from "./Create";
 
 
 
